d34437c9e6fcb0754ba4da614aef92fede3ba895
Rebecca <r.calvo.3921@vallauri.edu>
2026-09-14 09:45:03 +0200

    aggiornamento cartella

---
+++ 4/RICHIAMI_DI_JAVASCRIPT/Es_01_Oggetto_Date/index.js
@@ -0,0 +1,20 @@
+"use strict";
+	
+let txtData1 = document.getElementById("txtData1");
+let txtData2 = document.getElementById("txtData2");
+let button = document.getElementsByTagName("button")[0];
+let log = document.getElementById("log");
+
+assegnaDataAlTagInput()
+button.addEventListener("click", leggiDataDalTagInput)
+
+
+
+function assegnaDataAlTagInput(){
+	
+}
+
+
+function leggiDataDalTagInput(){
+
+}
